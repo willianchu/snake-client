4e0c9281d4f222ebaf195d7c0dded92b9f01f8c0
willianchu <willianchu@hotmail.com>
2022-01-20 00:16:55 -0800

    refactored and give a name to server ok

--- client.js
+++ client.js
@@ -6,24 +6,12 @@ const connect = function() {
     host: "172.17.193.35", // IP address here,
     port: "50541"// PORT number here,
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  conn.on("data", (data) => { // 01
-    console.log(`Server: ${data}`); // you ded cuz you idled
-  });
-
-  conn.on("connect", () => { // 02
-    console.log("Success!! connected to game server");
-    console.log(`Server: ${conn.remoteAddress}:${conn.remotePort}`);
-    conn.write("Name: WIL");
-  });
-
+  return conn;
 };
 
 module.exports = {connect};
-
 
 
 
--- play.js
+++ play.js
@@ -1,6 +1,16 @@
 const {connect} = require("./client");
 
 
+console.log(JSON.stringify(connect));
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
+conn.on("data", (data) => { // 01
+  console.log(`Server: ${data}`); // you ded cuz you idled
+});
+
+conn.on("connect", () => { // 02
+  console.log("Success!! connected to game server");
+  console.log(`Server: ${conn.remoteAddress}:${conn.remotePort}`);
+  conn.write("Name: CHU");
+});
