27a5f05a86e9906d33e6883de84ec6f58e0b5a71
willianchu <willianchu@hotmail.com>
2022-01-19 23:42:50 -0800

    slitted to module connection

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const net = require("net");
+const {connect} = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "172.17.193.35", // IP address here,
-    port: "50541"// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => { // 01
-    console.log(`Server: ${data}`); // you ded cuz you idled
-  });
-
-  conn.on("connect", () => { // 02
-    console.log("Success!! connected to game server");
-    console.log(`Server: ${conn.remoteAddress}:${conn.remotePort}`);
-    conn.write("Name: WIL");
-  });
-
-};
 
 console.log("Connecting ...");
 connect();
+
